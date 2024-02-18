15e3faff9f26f951cbfcecdc1c181674af2c9caf
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-18 12:05:33 -0800

    nav items with highlight mechanism

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,30 @@
+"use client";
+
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavItems = () => {
-  return <div>NavItems</div>;
+  return (
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link) => {
+        const pathname = usePathname();
+        const isActive = pathname === link.route;
+
+        return (
+          <li
+            key={link.route}
+            className={`${
+              isActive && `text-primary-500`
+            } flex-center p-medium-16 white-space-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default NavItems;
