f22d99bc86dc1e46fc7c9f65956a9535e9d2e42a
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-18 13:42:32 -0800

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t ">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2023 Evently. All Rights Reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
