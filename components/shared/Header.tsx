8dc2027ac257fd5119f943369abcb44d04c841f2
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-18 11:10:25 -0800

    add clerk and auth UI

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,45 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    // create header
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        {/* home logo */}
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently Logo"
+          />
+        </Link>
+        {/* clerk authetication dependent display */}
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
