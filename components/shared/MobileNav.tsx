bd38b3a46eacd01badf9c86458012dff56cbdd58
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-18 11:29:50 -0800

    add nav chartsheet

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import Image from "next/image";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetDescription,
+} from "../ui/sheet";
+import { Separator } from "../ui/separator";
+import NavItems from "./NavItems";
 
 const MobileNav = () => {
-  return <nav>Hi</nav>;
+  return (
+    <Sheet>
+      <SheetTrigger className="align-middle">
+        <Image
+          src="/assets/icons/menu.svg"
+          alt="menu"
+          width={24}
+          height={24}
+          className="cursor-pointer"
+        />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={128}
+          height={38}
+        />
+        <Separator className="border border-gray-50" />
+        <NavItems />
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
