0f8262ff3cb4a956129fe69405a86fecc0528f75
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-19 22:58:31 -0800

    fix misnamed mongodb url variable

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
