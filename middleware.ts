23ff1a121a46cf9340f3d210a9e2f6257156c3b4
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-19 23:41:15 -0800

    attempt to fix webhook

--- middleware.ts
+++ middleware.ts
@@ -5,15 +5,15 @@ export default authMiddleware({
   publicRoutes: [
     "/",
     "/events/:id",
-    "/api/webhooks/clerk",
-    "/api/webhooks/strip",
+    "/api/webhook/clerk",
+    "/api/webhook/strip",
     "/api/uploadthing",
   ],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: [
-    "/api/webhooks/clerk",
-    "/api/webhooks/stripe",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
     "/api/uploadthing",
   ],
 });
