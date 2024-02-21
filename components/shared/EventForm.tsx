4ae1c008cd63b91ccb6d9ab180b32e28c4cc97b8
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-20 21:17:22 -0800

    create and update dummy pages

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+};
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+  return <div>EventForm</div>;
+};
+
+export default EventForm;
