a20b69bb1a4f411b0b3ac88018cbba2385c9e142
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-21 13:55:03 -0800

    fix checkbox button

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -248,6 +248,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                                 Free Ticket
                               </label>
                               <Checkbox
+                                onCheckedChange={field.onChange}
+                                checked={field.value}
                                 id="isFree"
                                 className="mr-2 h-5 w-5 border-2 border-primary-500"
                               />
