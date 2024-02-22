fa65170218eb8197319e10de696d88d19d1ad9f1
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-21 21:53:56 -0800

    card view for each event

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
         </div>
 
         <Collection
-          data={[]}
+          data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
