7cfe8ce7439970e7201b5953fca4de2579e36429
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-22 12:13:18 -0800

    display ticket purhcased info

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 import React from "react";
 import { auth } from "@clerk/nextjs";
 import { getEventsByUser } from "@/lib/actions/event.action";
+import { getOrdersByUser } from "@/lib/actions/order.action";
+import { IOrder } from "@/lib/database/models/order.model";
 
 const ProfilePage = async () => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
+
+  const orders = await getOrdersByUser({ userId, page: 1 });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event || []);
 
   const organizedEvents = await getEventsByUser({ userId, page: 1 });
 
@@ -23,9 +28,9 @@ const ProfilePage = async () => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
-          data={events?.data}
+          data={orderedEvents}
           emptyTitle="No event tickets purhcased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
@@ -34,12 +39,12 @@ const ProfilePage = async () => {
           urlParamName="orderPage"
           totalPages={2}
         />
-      </section> */}
+      </section>
 
       {/* events organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <h3 className="h3-bold text-center sm:text-left">My Events</h3>
           <Button asChild size="lg" className="button hidden sm:flex">
             <Link href="/events/create">Explore More Events</Link>
           </Button>
