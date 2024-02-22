fd2208441c05aff7e6f074d0f5756a5cdf61db79
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-21 20:01:45 -0800

    placeholder for collection

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
@@ -39,6 +40,15 @@ export default function Home() {
         <div className="flex w-full felx-col gap-5 md:flex-row">
           Search CategoryFilter
         </div>
+
+        <Collection
+          data={[]}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+        />
       </section>
     </>
   );
--- lib/actions/event.action.ts
+++ lib/actions/event.action.ts
@@ -1,5 +1,5 @@
 "use server";
-import { CreateEventParams } from "@/types";
+import { CreateEventParams, GetAllEventsParams } from "@/types";
 import { connectToDatabase } from "../database";
 import { handleError } from "../utils";
 import User from "../database/models/user.model";
@@ -51,6 +51,28 @@ export const getEventById = async (eventId: string) => {
     if (!event) {
       throw new Error("Event not found");
     }
+
+    return JSON.parse(JSON.stringify(event));
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllEvents = async ({
+  query,
+  limit = 6,
+  page,
+  category,
+}: GetAllEventsParams) => {
+  try {
+    await connectToDatabase();
+
+    const conditions = {};
+
+    const eventsQuery = Event.find(conditions)
+      .sort({ createdAt: "desc" })
+      .skip(0)
+      .limit(limit);
 
     return JSON.parse(JSON.stringify(event));
   } catch (error) {
