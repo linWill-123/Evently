aad812f64bd5e918ed6644a86ff0030b15e593de
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-21 23:46:46 -0800

    profile page

---
+++ app/(root)/profile/page.tsx
@@ -0,0 +1,65 @@
+import { Button } from "@/components/ui/button";
+import Collection from "@/components/shared/Collection";
+import Link from "next/link";
+import React from "react";
+import { auth } from "@clerk/nextjs";
+import { getEventsByUser } from "@/lib/actions/event.action";
+
+const ProfilePage = async () => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+
+  return (
+    <>
+      {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">Explore More Events</Link>
+          </Button>
+        </div>
+      </section>
+
+      {/* <section className="wrapper my-8">
+        <Collection
+          data={events?.data}
+          emptyTitle="No event tickets purhcased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={6}
+          page={1}
+          urlParamName="orderPage"
+          totalPages={2}
+        />
+      </section> */}
+
+      {/* events organized */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">Explore More Events</Link>
+          </Button>
+        </div>
+      </section>
+
+      <section className="wrapper my-8">
+        <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No events created yet"
+          emptyStateSubtext="Go create some now!"
+          collectionType="Events_Organized"
+          limit={6}
+          page={1}
+          urlParamName="eventsPage"
+          totalPages={2}
+        />
+      </section>
+    </>
+  );
+};
+
+export default ProfilePage;
