22fdec2a76bb2d86165d0d3f822091fbc529a052
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-22 15:08:13 -0800

    functioning search bar logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,18 @@ import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.action";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query: "",
-    category: "",
+    query: searchText,
+    category,
     page: 1,
     limit: 6,
   });
