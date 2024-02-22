ef7611223e37d08f419244ecc17b8e059a82f6c7
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-22 11:12:39 -0800

    stripe checkout

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -2,9 +2,38 @@ import { IEvent } from "@/lib/database/models/event.model";
 import React from "react";
 import { Button } from "../ui/button";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { checkoutOrder } from "@/lib/actions/order.action";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  React.useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready."
+      );
+    }
+  }, []);
+
   const onCheckout = async () => {
-    console.log("Checkout");
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+    };
+
+    await checkoutOrder(order);
   };
   return (
     <form action={onCheckout} method="post">
