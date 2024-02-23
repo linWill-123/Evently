3bdb405ef370de720f3ee6f13c064ecd3cffc908
WillLIn1341425 <66269994+WillLIn1341425@users.noreply.github.com>
2024-02-22 16:41:56 -0800

    fix pagination in other collections

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -103,8 +103,8 @@ const EventDetails = async ({
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
-          page={1}
+          limit={3}
+          page={searchParams.page as string}
           totalPages={relatedEvents?.totalPages}
         />
       </section>
